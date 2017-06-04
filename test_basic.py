0e45804a7eefe2e59fa1b86640de1958834e619f
Vincent Guarnaccia <Vincent.Guarnaccia@gmail.com>
2017-06-04 14:24:27 -0700

    comment out not implemented tests and made tests style consistent

--- test_basic.py
+++ test_basic.py
@@ -29,14 +29,15 @@ class test_todo_finder_single_line_comments(unittest.TestCase):
 
     def test_strip_leading_space_no_author(self):
         bad_whitespace = '# TODO:             \t content?'
-        first_only, *_ = srctasks.todo_finder(bad_whitespace)
-        self.assertEqual(first_only.task, 'content?')
+        answer = [srctasks.Todo(author='', task='content?')]
+        todos = srctasks.todo_finder(bad_whitespace)
+        self.assertEqual(todos, answer)
 
     def test_strip_leading_space_with_author(self):
         bad_whitespace = '# TODO (  programmer ):             \t content?'
-        first_only, *_ = srctasks.todo_finder(bad_whitespace)
-        self.assertEqual(first_only.task, 'content?')
-        self.assertEqual(first_only.author, 'programmer')
+        answer = [srctasks.Todo(author='programmer', task='content?')]
+        todos = srctasks.todo_finder(bad_whitespace)
+        self.assertEqual(todos, answer)
 
     def test_multiple_todos_no_author(self):
         many_todos = """lorem
@@ -50,13 +51,13 @@ class test_todo_finder_single_line_comments(unittest.TestCase):
 
         -- todo: a third task
         """
-        first, second, third = srctasks.todo_finder(many_todos)
-        self.assertEqual(first.task, 'task1')
-        self.assertEqual(first.author, 'me')
-        self.assertEqual(second.task, 'add another task')
-        self.assertEqual(second.author, '')
-        self.assertEqual(third.task, 'a third task')
-        self.assertEqual(third.author, '')
+        answer = [
+            srctasks.Todo(author='me', task='task1'),
+            srctasks.Todo(author='', task='add another task'),
+            srctasks.Todo(author='', task='a third task')
+        ]
+        todos = srctasks.todo_finder(many_todos)
+        self.assertEqual(todos, answer)
 
     def test_many_tokens(self):
         many_tokens = """# todo ( person ): task
@@ -66,21 +67,16 @@ class test_todo_finder_single_line_comments(unittest.TestCase):
         # HACK: please don't do this one
         -- NOTE (good programmer) : I can leave notes.
         """
-        todos = srctasks.todo_finder(
-            many_tokens, tokens='todo fixme changed xxx hack note')
-        first, second, third, fourth, fifth, sixth = todos
-        self.assertEqual(first.task, 'task')
-        self.assertEqual(first.author, 'person')
-        self.assertEqual(second.task, 'fix')
-        self.assertEqual(second.author, 'fixer')
-        self.assertEqual(third.task, 'a change')
-        self.assertEqual(third.author, '')
-        self.assertEqual(fourth.task, 'oops, a mistake')
-        self.assertEqual(fourth.author, '')
-        self.assertEqual(fifth.task, 'please don\'t do this one')
-        self.assertEqual(fifth.author, '')
-        self.assertEqual(sixth.task, 'I can leave notes.')
-        self.assertEqual(sixth.author, 'good programmer')
+        answer = [
+            srctasks.Todo(author='person', task='task'),
+            srctasks.Todo(author='fixer', task='fix'),
+            srctasks.Todo(author='', task='a change'),
+            srctasks.Todo(author='', task='oops, a mistake'),
+            srctasks.Todo(author='', task='please don\'t do this one'),
+            srctasks.Todo(author='good programmer', task='I can leave notes.')
+        ]
+        todos = srctasks.todo_finder(many_tokens, tokens='todo fixme changed xxx hack note')
+        self.assertEqual(todos, answer)
 
     def test_case_sensitive(self):
         tasks = """This is some code
@@ -91,23 +87,23 @@ class test_todo_finder_single_line_comments(unittest.TestCase):
         self.assertEqual(todos, answer)
 
 
-class test_todo_finder_multiline_comments(unittest.TestCase):
+# class test_todo_finder_multiline_comments(unittest.TestCase):
 
-    def test_across_multiple_lines(self):
-        test = '''
-            TODO:
-                * implement this
-                * test it
-                * document it
-        '''
+#     def test_across_multiple_lines(self):
+#         test = '''
+#             TODO:
+#                 * implement this
+#                 * test it
+#                 * document it
+#         '''
 
-        answer = [
-            srctasks.Todo(author='', task='implement this'),
-            srctasks.Todo(author='', task='test it'),
-            srctasks.Todo(author='', task='document it'),
-        ]
-        todos = srctasks.todo_finder(test)
-        self.assertEqual(todos, answer)
+#         answer = [
+#             srctasks.Todo(author='', task='implement this'),
+#             srctasks.Todo(author='', task='test it'),
+#             srctasks.Todo(author='', task='document it'),
+#         ]
+#         todos = srctasks.todo_finder(test)
+#         self.assertEqual(todos, answer)
 
 #     def test_on_same_line(self):
 #         test = '''
